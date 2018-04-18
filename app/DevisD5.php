98d5e543df695c5c85e809247207ec004fa23f43
CeliaCha <chazel.celia@gmail.com>
2018-04-18 14:02:18 +0200

    Migrations Celia

---
+++ app/DevisD5.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DevisD5 extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/DevisD4Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DevisD4Controller extends Controller
+{
+    //
+}
