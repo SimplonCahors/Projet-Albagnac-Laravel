203ed3f9708cbf19dd984cd8f175b608accad9f0
Sophiequehen <sophie.quehen@gmail.com>
2018-04-19 15:52:55 +0200

    finalisation de la page archives selon le wireframe

--- resources/views/archives.blade.php
+++ resources/views/archives.blade.php
@@ -8,18 +8,49 @@
 @endsection
 
 @section('content')
-<form class="form-inline">
-	<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-</form>
+<div class="archives container">
+	<form class="form-inline">
+		<input class="archives form-control mr-sm-2" type="search" placeholder="Rechercher un DSO..." aria-label="Search">
+	</form>
 
-<div class="list-group">
-	<a href="#" class="list-group-item list-group-item-action active">
-		Cras justo odio
-	</a>
-	<a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-	<a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-	<a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-	<a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+	<div class="archives filtresetliste">
+		<div class="archives btn-group-vertical">
+			<button id="trierpar">TRIER PAR</button>
+			<select class="custom-select" id="inputGroupSelect01">
+				<option selected>Commercial</option>
+				<option value="1">Dante</option>
+				<option value="1">Virgil</option>
+				<option value="2">Pluton</option>
+				<option value="3">MacGyver</option>
+			</select>
+			<select class="custom-select" id="inputGroupSelect02">
+				<option selected>Date</option>
+				<option value="1">Hier</option>
+				<option value="2">Aujourd'hui</option>
+				<option value="3">Demain</option>
+			</select>
+			<select class="custom-select" id="inputGroupSelect03">
+				<option selected>Référence</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+			</select>
+			<select class="custom-select" id="inputGroupSelect04">
+				<option selected>Client</option>
+				<option value="1">Georges</option>
+				<option value="2">Jackson</option>
+				<option value="3">Henri</option>
+			</select>
+		</div>
+
+		<div class="archives list-group">
+			<a href="#" class="archives list-group-item list-group-item-action disabled">
+				LISTE DES DSO
+			</a>
+			<a href="#" class="archives list-group-item list-group-item-action">Un autre nom de DSO</a>
+			<a href="#" class="archives list-group-item list-group-item-action">Encore un nom de DSO</a>
+			<a href="#" class="archives list-group-item list-group-item-action">Encore un autre nom de DSO</a>
+		</div>
+	</div>
 </div>
 @endsection
