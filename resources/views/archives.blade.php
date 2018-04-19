b6d1c1ed4801b22b935407a805cd7fbef253cc75
Sophiequehen <sophie.quehen@gmail.com>
2018-04-19 11:34:59 +0200

    ajout des composants sur la view archive

--- resources/views/archives.blade.php
+++ resources/views/archives.blade.php
@@ -3,10 +3,23 @@
 @section('title', 'Archives')
 
 @section('sidebar')
-    @parent
-   
+@parent
+
 @endsection
 
 @section('content')
-  
+<form class="form-inline">
+	<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+</form>
+
+<div class="list-group">
+	<a href="#" class="list-group-item list-group-item-action active">
+		Cras justo odio
+	</a>
+	<a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+	<a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+	<a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+	<a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+</div>
 @endsection
