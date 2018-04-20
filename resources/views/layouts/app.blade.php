133162c260c29cce0469247f1b68522413ea7a8d
Sophiequehen <sophie.quehen@gmail.com>
2018-04-20 14:24:33 +0200

    ajout de la sous-nav sur les pages etiquettes et emballages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     </header>
 
     <!-- Pour intégrer la sous-nav uniquement sur les pages choisies, l'url entre les quotes -->
-    @if (Request::is('nouveau_dso'))
+    @if (Request::is('nouveau_dso') or Request::is('etiquettes') or Request::is('emballages'))
     @include('sous_nav')
     @endif
 
