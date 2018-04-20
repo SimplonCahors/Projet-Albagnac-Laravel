5d66e78315e2953acad0c577e2a3ec3edd0a7525
echayotte <echayotte@gmail.com>
2018-04-20 20:22:43 +0200

    mise  à jour Routes:: -v1

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,38 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/*
+|--------------------------------------------------------------------------
+| Bonnes routes
+|--------------------------------------------------------------------------
+*/
+
+//infos-admin
+Route::get('/dso/{idDso}/devis/infos-admin', 'DsorootController@index')->name('infos-admin');
+Route::get('/dso/{idDso}/devis/infos-admin/create', 'DsorootController@create')->name('infos-admin_create');
+
+
+// emballages
+Route::get('/dso/{idDso}/emballages', 'EmballageController@index');
+Route::get('/dso/{idDso}/emballages/create', 'EmballageController@create');
+Route::get('/dso/{idDso}/emballages/{idEmballages}', 'EmballageController@show');
+Route::post('/dso/{idDso}/emballages', 'EmballageController@store');
+
+
+// etiquettes
+Route::get('/dso/{idDso}/etiquettes', 'EtiquetteController@index');
+Route::get('/dso/{idDso}/etiquettes/create', 'EtiquetteController@create');
+Route::get('/dso/{idDso}/etiquettes/{idEtiquettes}', 'EtiquetteController@show');
+Route::post('/dso/{idDso}/etiquettes', 'EtiquetteController@store');
+
+
+/*
+|--------------------------------------------------------------------------
+| routes du front à redispatcher
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/accueil', function () {
     return view('accueil');
 });
@@ -42,14 +74,14 @@ Route::get('/layout', function () {
     return view('layout');
 });
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
+/*
+|--------------------------------------------------------------------------
+| route à vérifier
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
     $dsoroots = \App\Dsoroot::all();
     return view('welcome')->with('dsoroots', $dsoroots);
 });
-
-// Route::get('/dso/{idDso}/devis/infos-admin', 'DsorootController@index')->name('infos-admin');
-Route::get('/dso/{idDso}/devis/infos-admin/create', 'DsorootController@create')->name('infos-admin_create');
