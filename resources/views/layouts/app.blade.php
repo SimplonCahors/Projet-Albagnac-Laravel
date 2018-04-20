6c282c4a4801ee0f8c73f898dff4bb6de92bb122
Sophiequehen <sophie.quehen@gmail.com>
2018-04-20 13:40:32 +0200

    ajout de la condition dans l'app.blade pour que la sous-nav n'apparaîsse que sur les pages souaitées

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,26 @@
 <html>
-    <head>
-        <title>@yield('Accueil')</title>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    </head>
-    <body>
-        <!-- @section('sidebar') -->
-        <header>
-            <h2>@yield('title')</h2>
-            <h5 style="text-align:right; margin-top:30px; margin-right:30px;"><a href="/">Retour</a></h5>
-        </header>
-        @include('sous_nav')
-        @show
+<head>
+    <title>@yield('Accueil')</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <!-- @section('sidebar') -->
+    <header>
+        <h2>@yield('title')</h2>
+        <h5 style="text-align:right; margin-top:30px; margin-right:30px;"><a href="/">Retour</a></h5>
+    </header>
 
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+    <!-- Pour intégrer la sous-nav uniquement sur les pages choisies, l'url entre les quotes -->
+    @if (Request::is('archives'))
+    @include('sous_nav')
+    @endif
+
+    @show
+
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
 </html>
 
--- resources/views/nouveau_dso.blade.php
+++ resources/views/nouveau_dso.blade.php
@@ -2,41 +2,43 @@
 
 @section('title', 'Nouveau DSO')
 
+@include('sous_nav')
+
 @section('content')
 <section id="nouveau_dso">
-<form method="POST">
-	<div class="div_dso">
-		<div id="left">
-			<div class="nouveau-dso form-group">
-				<label for="exampleInputEmail1">Date Première demande client :</label>
-				<input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+	<form method="POST">
+		<div class="div_dso">
+			<div id="left">
+				<div class="nouveau-dso form-group">
+					<label for="exampleInputEmail1">Date Première demande client :</label>
+					<input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+				</div>
+				<div class="nouveau-dso form-group">
+					<label for="exampleInputPassword1">Date de la réponse souhaitée :</label>
+					<input type="date" class="form-control" id="exampleInputPassword1">
+				</div>
+				<div class="nouveau-dso form-group">
+					<label for="exampleInputEmail1">Date de l'envoi de l'offre :</label>
+					<input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				</div>
+				<div class="nouveau-dso form-group">
+					<label for="exampleInputPassword1">Date de livraison souhaitée :</label>
+					<input type="date" class="form-control" id="exampleInputPassword1">
+				</div>
 			</div>
-			<div class="nouveau-dso form-group">
-				<label for="exampleInputPassword1">Date de la réponse souhaitée :</label>
-				<input type="date" class="form-control" id="exampleInputPassword1">
-			</div>
-			<div class="nouveau-dso form-group">
-				<label for="exampleInputEmail1">Date de l'envoi de l'offre :</label>
-				<input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-			</div>
-			<div class="nouveau-dso form-group">
-				<label for="exampleInputPassword1">Date de livraison souhaitée :</label>
-				<input type="date" class="form-control" id="exampleInputPassword1">
-			</div>
-		</div>
 
-		<div id="right">
-			<div class="nouveau-dso form-group">
-				<label for="exampleInputEmail1">Date création fiche :</label>
-				<input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+			<div id="right">
+				<div class="nouveau-dso form-group">
+					<label for="exampleInputEmail1">Date création fiche :</label>
+					<input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				</div>
+				<div class="nouveau-dso form-group">
+					<label for="exampleInputPassword1">Nom du commercial :</label>
+					<input type="text" class="form-control" id="exampleInputPassword1" required>
+				</div>
+				<button type="submit">Suivant ></button>
 			</div>
-			<div class="nouveau-dso form-group">
-				<label for="exampleInputPassword1">Nom du commercial :</label>
-				<input type="text" class="form-control" id="exampleInputPassword1" required>
-			</div>
-			<button type="submit">Suivant ></button>
 		</div>
-	</div>
-</form>
+	</form>
 </section>
 @endsection
