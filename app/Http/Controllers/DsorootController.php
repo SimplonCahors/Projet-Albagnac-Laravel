df77f4ecc98b53361af8f360d3a2e6117dd37b26
CeliaCha <chazel.celia@gmail.com>
2018-04-23 15:00:40 +0200

    Add create dsoroot

--- app/Http/Controllers/DsorootController.php
+++ app/Http/Controllers/DsorootController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dsoroot;
 
 class DsorootController extends Controller
 {
@@ -13,7 +14,7 @@ class DsorootController extends Controller
      */
     public function index()
     {
-        return view('dso.devis.roots.index');
+        // return view('dso.devis.roots.index'); // inutile
     }
 
     /**
@@ -21,9 +22,9 @@ class DsorootController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($idUser)
     {
-        return view('dso.devis.roots.create');
+        return view('dso.devis.roots.create')->with('idUser', $idUser) ; // le chemin semble ok
     }
 
     /**
@@ -34,14 +35,31 @@ class DsorootController extends Controller
      */
     public function store(Request $request)
     {
+        
         $this->validate(request(), [
-
-            'title' => 'required',
-            'body' => 'required'
+            
+            'date_demande' => 'required',
+            'date_rep' => 'required',
+            'date_envoi' => 'required',
+            'date_livraison' => 'required',
         ]);
 
+            //create a new emballage 
+            $dsoRoot = new Dsoroot;
+
+            //use the request data
+            $dsoRoot->id_user = request('id_user');
+            $dsoRoot->date_demande = request('date_demande');
+            $dsoRoot->date_rep = request('date_rep');
+            $dsoRoot->date_envoi = request('date_envoi');
+            $dsoRoot->date_livraison = request('date_livraison');
+
+
+            //save it to the dbb
+            $dsoRoot->save();
+
         //create a new post using the request data and save it to the dbb
-        Dsoroot::create(request(['title', 'body']));
+        //Dsoroot::create(request(['date_demande', 'date_rep', 'date_envoi', 'date_livraison']));
 
         //redirect to the home page
         return redirect('/');
