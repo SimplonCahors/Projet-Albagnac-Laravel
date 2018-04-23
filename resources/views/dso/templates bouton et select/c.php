d99e652a9377acf6681f745aa0d1ba5bfc64ffc3
echayotte <echayotte@gmail.com>
2018-04-23 12:07:47 +0200

    aojut templates front boutons et select

---
+++ resources/views/dso/templates bouton et select/c.php
@@ -0,0 +1,63 @@
+ /* |------ | Section C-1 |------ */
+
+<div class="Class__Select__Div">
+    <button id="___">Matière de l'emballage</button>
+    <select class="Class__Select" id="matiere_emb" name="matiere_emb">
+        <option selected>Thème</option>
+        <option value="1">Value1</option>
+        <option value="1">Value2</option>
+        <option value="2">Value3</option>
+        <option value="3">Value4</option>
+    </select>
+</div>
+
+
+<div class="Class__Select__Div">
+    <button id="___">Type d'emballage</button>
+    <select class="Class__Select" id="type_emb" name="type_emb">
+        <option selected>Thème</option>
+        <option value="1">Value1</option>
+        <option value="1">Value2</option>
+        <option value="2">Value3</option>
+        <option value="3">Value4</option>
+    </select>
+</div>
+
+
+<div class="Class__Input__Div">
+    <label for="nbr_emb">Nombre d'emballage</label>
+    <input type="___" class="Class__Input" id="nbr_emb" name="nbr_emb">
+</div>
+
+
+
+/* |------ | Section C-2 |------ */
+
+<div class="Class__Select__Div">
+    <button id="___">Matière de le l'étiquette</button>
+    <select class="Class__Select" id="matiere_eti" name="matiere_eti">
+        <option selected>Thème</option>
+        <option value="1">Value1</option>
+        <option value="1">Value2</option>
+        <option value="2">Value3</option>
+        <option value="3">Value4</option>
+    </select>
+</div>
+
+
+<div class="Class__Select__Div">
+    <button id="___">Type de l'étiquette</button>
+    <select class="Class__Select" id="type_eti" name="type_eti">
+        <option selected>Thème</option>
+        <option value="1">Value1</option>
+        <option value="1">Value2</option>
+        <option value="2">Value3</option>
+        <option value="3">Value4</option>
+    </select>
+</div>
+
+
+<div class="Class__Input__Div">
+    <label for="nbr_eti">Nombre de l'étiquette</label>
+    <input type="___" class="Class__Input" id="nbr_eti" name="nbr_eti">
+</div>
---
+++ resources/views/dso/templates bouton et select/template_Btn_Select.php
@@ -0,0 +1,17 @@
+ /* |------ | Section X-1 |------ */
+
+<div class="Class__Input__Div">
+    <label for="__OO__">TexteARemplir</label>
+    <input type="___" class="Class__Input" id="__OO__" name="__OO__">
+</div>
+
+<div class="Class__Select__Div">
+    <button id="___">TexteARemplir</button>
+    <select class="Class__Select" id="__OO__" name="__OO__">
+        <option selected>Thème</option>
+        <option value="1">Value1</option>
+        <option value="1">Value2</option>
+        <option value="2">Value3</option>
+        <option value="3">Value4</option>
+    </select>
+</div>
