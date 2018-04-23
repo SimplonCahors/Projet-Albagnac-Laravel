1f8bfddb0bca943a3df736bcb60c32c94ba83eff
Sophiequehen <sophie.quehen@gmail.com>
2018-04-23 16:43:29 +0200

    responsive pour le template de la page a

--- resources/views/dso/devis/b/create.blade.php
+++ resources/views/dso/devis/b/create.blade.php
@@ -6,7 +6,7 @@
 <section id="nouveau_dso">
 	<form method="POST" action="#">
 		@csrf
-		 /* |------ | Section b-1 |------ */
+		/* |------ | Section b-1 |------ */
 
 		<div class="div_info">
 			<div id="left">
@@ -14,16 +14,16 @@
 					<label>Besoin :</label>
 					<input type="text" class="form-control">
 
-						<div class="Class__Select__Div">
-						   <!--  <button id="___">TexteARemplir</button> -->
-						    <select class="Class__Select" id="__OO__" name="__OO__">
-						        <option selected>Choisir</option>
-						        <option value="1">Value1</option>
-						        <option value="1">Value2</option>
-						        <option value="2">Value3</option>
-						        <option value="3">Value4</option>
-						    </select>
-						</div>
+					<div class="Class__Select__Div">
+						<!--  <button id="___">TexteARemplir</button> -->
+						<select class="Class__Select" id="__OO__" name="__OO__">
+							<option selected>Choisir</option>
+							<option value="1">Value1</option>
+							<option value="1">Value2</option>
+							<option value="2">Value3</option>
+							<option value="3">Value4</option>
+						</select>
+					</div>
 
 					<label for="exampleInputEmail1">Remarques :</label>
 					<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
@@ -39,32 +39,32 @@
 					<input type="text" placeholder="remarques :" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 				</div>
 
-				 /* |------ | Section b-2 |------ */
-
-	
-
-			<div id="right">
-				<div class="nouveau-dso form-group">
-					<label for="exampleInputEmail1">Nombre et type potentiel de machines :</label>
-					<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
-				</div>
-				<div class="nouveau-dso form-group">
-					<label for="exampleInputPassword1">Site de production :</label>
-					<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
-				</div>
-				<div class="nouveau-dso form-group">
-					<label for="exampleInputPassword1">Implantation des sites en cas de groupe industriel :</label>
-					<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
-				</div>
-				<div class="nouveau-dso form-group">
-					<label for="exampleInputPassword1">Environnement concurrentiel lié à la demande de devis</label>
-					<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
-				</div>
+				/* |------ | Section b-2 |------ */
 
 				
-				<button type="button" class="btn btn-warning" type="submit">Suivant ></button>
+
+				<div id="right">
+					<div class="nouveau-dso form-group">
+						<label for="exampleInputEmail1">Nombre et type potentiel de machines :</label>
+						<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
+					</div>
+					<div class="nouveau-dso form-group">
+						<label for="exampleInputPassword1">Site de production :</label>
+						<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
+					</div>
+					<div class="nouveau-dso form-group">
+						<label for="exampleInputPassword1">Implantation des sites en cas de groupe industriel :</label>
+						<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
+					</div>
+					<div class="nouveau-dso form-group">
+						<label for="exampleInputPassword1">Environnement concurrentiel lié à la demande de devis</label>
+						<textarea type="text" class="form-control" id="exampleInputPassword1" required></textarea>
+					</div>
+
+					
+					<button type="button" class="btn btn-warning" type="submit">Suivant ></button>
+				</div>
 			</div>
-		</div>
-	</form>
-</section>
-@endsection
+		</form>
+	</section>
+	@endsection
