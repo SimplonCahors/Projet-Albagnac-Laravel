9b8fc991bb30abd902e3522311e2939199260c01
nico <nicocofer@hotmail.com>
2018-04-24 15:04:21 +0200

    ajour de la method destroy sur le controller Emballage

--- resources/views/dso/emballages/index.blade.php
+++ resources/views/dso/emballages/index.blade.php
@@ -11,7 +11,9 @@
 	<ul>
 		<a href="/dso/{{ $idDso }}/emballages/create/">Ajouter un emballage</a>
 		@foreach($emballages as $emballage)
-		<li>{{ $emballage->ref_int }}</li><a href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/edit" id="{{ $emballage->id }}">edit du n°{{ $emballage->id }}</a>
+		<li>{{ $emballage->ref_int }}</li>
+		<a href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/edit">edit du n°{{ $emballage->id }}</a>
+		<a href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/destroy">delete du n°{{ $emballage->id }}</a>
 		@endforeach
 	</ul>
 
