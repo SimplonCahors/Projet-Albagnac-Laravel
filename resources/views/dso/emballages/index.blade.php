714caf13cf3511199c50c87d37e116d465e5040c
Sophiequehen <sophie.quehen@gmail.com>
2018-04-25 14:29:49 +0200

    revue de code pour etiquettes et emballages

--- resources/views/dso/emballages/index.blade.php
+++ resources/views/dso/emballages/index.blade.php
@@ -7,34 +7,30 @@
 @endsection
 
 @section('content')
-<<<<<<< HEAD
 @include('sous_nav')
-<section id="liste_etiquettes">
-	<div class="liste_etiqu">
-		=======
-		<section id="liste_emballages">
-			<div class="liste_emball">
-				>>>>>>> front_DDD
-				<ul>
-					@foreach($emballages as $emballage)
-					<li>
-						<div class="card">
-							<h5 class="card-header text-center">Emballage n°{{ $emballage->id }}</h5>
-							<div class="card-body">
-								<h5 class="card-title">Infos sur l'emballage</h5>
-								<div class="icons">
-									<a  href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/edit"><i class="medium material-icons">create</i></a>
-									<a href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/destroy"><i class="medium material-icons">delete</i></a>
-								</div>
-							</div>
+
+<section id="liste_emballages">
+	<div class="liste_emball">
+		<ul>
+			@foreach($emballages as $emballage)
+			<li>
+				<div class="card">
+					<h5 class="card-header text-center">Emballage n°{{ $emballage->id }}</h5>
+					<div class="card-body">
+						<h5 class="card-title">Infos sur l'emballage</h5>
+						<div class="icons">
+							<a  href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/edit"><i class="medium material-icons">create</i></a>
+							<a href="/dso/{{ $idDso }}/emballages/{{ $emballage->id }}/destroy"><i class="medium material-icons">delete</i></a>
 						</div>
-					</li>
-					@endforeach
-				</ul>
-				<div class="emball_buttons">
-					<a href="/dso/{{ $idDso }}/emballages/create/" role="button" class="btn btn-primary">Ajouter un emballage</a>
-					<a role="button" class="btn btn-primary">Suivant ></a>
+					</div>
 				</div>
-			</div>
-		</section>
-		@endsection
+			</li>
+			@endforeach
+		</ul>
+		<div class="emball_buttons">
+			<a href="/dso/{{ $idDso }}/emballages/create/" role="button" class="btn btn-primary">Ajouter un emballage</a>
+			<a role="button" class="btn btn-primary">Suivant ></a>
+		</div>
+	</div>
+</section>
+@endsection
