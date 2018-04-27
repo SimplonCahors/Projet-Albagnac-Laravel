f3c60dce775e9b598a09351e3582da64987df9eb
Gloner <antoine.gloner@orange.frgit config --global user.name Antoinegit user.email antoine.gloner@orange.fr>
2018-04-27 10:15:02 +0200

    correction de la view

--- resources/views/dso/devis/e/index.blade.php
+++ resources/views/dso/devis/e/index.blade.php
@@ -13,6 +13,46 @@
 		@csrf
 		<div id="accordion">
 			<div class="card">
+				<div class="card">
+					<div class="card-header" id="headingThree">
+						<h5 class="mb-0">
+							<a role="button"  class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								MATÉRIELS IMPOSÉS / IMPRESSION
+							</a>
+						</h5>
+					</div>
+					<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+						<div class="card-body">
+							<section id="liste_etiquettes">
+	<div class="liste_etiqu">
+		<ul>
+			<li>
+				<div class="card">
+					<h5 class="card-header text-center">FICHE n°XXX</h5>
+					<div class="card-body">
+						<div><h5 class="card-title">Information</h5></div>
+						<div class="icons">
+							<a><i class="medium material-icons">create</i></a>
+							<a><i class="medium material-icons">delete</i></a>
+						</div>
+					</div>
+				</div>
+			</li>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<button type="button" class="btn btn-primary" type="submit">Valider ></button>
+		</div>
+	</form>
+</section>
+
+<!-- <section id="a">
+	<form method="POST" action="">
+		@csrf
+		<div id="accordion">
+			<div class="card">
 				<div class="card-header" id="headingOne">
 					<h5 class="mb-0">
 						<a role="button"  class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -28,11 +68,6 @@
 								@foreach($devisE1 as $devisx)
 								<li>
 									<div class="card">
-	
-
-
-
-
 									<h5 class="card-header text-center">Matériel imposé par le client</h5>
 										<div class="card-body">
 											<h5 class="card-title">Infos sur l'emballage</h5>
@@ -87,7 +122,7 @@
 				</div>
 			</div>
 			<button type="button" class="btn btn-primary" type="submit">Valider ></button>
-		</div>
+		</div> -->
 
 
 
