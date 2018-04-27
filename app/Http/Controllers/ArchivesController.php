6f70abcc9a0f888011ddb51a95224bce1852078a
echayotte <echayotte@gmail.com>
2018-04-27 13:50:00 +0200

    suppression return view 'archives' de index

--- app/Http/Controllers/ArchivesController.php
+++ app/Http/Controllers/ArchivesController.php
@@ -8,44 +8,35 @@ use App\Dsoroot;
 use App\User;
 use Auth;
 
-
 class ArchivesController extends Controller
 {
-
     public function index()
     {
         $dsoList = Dsoroot::all();   // va chercher toutes les infos de la table Dsoroot
         $idUser = Auth::user()->id ;  // permet de recup l'id de l'utilisateur connecté
         $users = User::all();   // permet de recup les infos de la table Users
 
-        $dsocom = Dsoroot::all()->where('id_user', $idUser);  // permet de recupérer les dso du com connecté
+        $dsocom = Dsoroot::all()->where('id_user', $idUser);  // permet de recupérer les dso du com ou adv connecté
 
-        $dsovalide =Dsoroot::all()->where('termine', true)->where('valide', true);// permet de recupérer les dso terminé pour l'adv 
+        $dsovalide =Dsoroot::all()->where('termine', true)->where('valide', true);// permet de recupérer les dso terminé pour l'adv
 
         // return view('accueil')->with('dsoList', $dsoList)->with('idUser', $idUser) ;
         return view('archives', ['dsoList'=>$dsoList, 'idUser'=> $idUser, 'users'=>$users, 'dsocom'=>$dsocom, 'dsovalide'=>$dsovalide]);
     }
-    // {
-    //     return view('archives');
-    // }
 
     public function create($idDso)
     {
-
     }
 
-    public function store($idDso, Request $request) 
+    public function store($idDso, Request $request)
     {
-
     }
 
-    public function edit($idDso) 
+    public function edit($idDso)
     {
-
     }
 
     public function update($idDso, Request $request)
     {
-
     }
 }
