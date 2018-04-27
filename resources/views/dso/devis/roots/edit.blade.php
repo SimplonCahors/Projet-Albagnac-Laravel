39fc2e6d8228fc90141c45bfde243527e01d1aea
nico <nicocofer@hotmail.com>
2018-04-27 14:26:07 +0200

    route acceder au Dso de l'accueil réparée

--- resources/views/dso/devis/roots/edit.blade.php
+++ resources/views/dso/devis/roots/edit.blade.php
@@ -1,36 +1,44 @@
 @extends('layouts.app')
+
+@section('title', 'Nouveau DSO')
+
 @section('content')
-<div class="row">
-                <div class="col-md-8 order-md-1">
-                    <h4 class="mb-3">DSO N° {{$idDso}}</h4>
-                    <form class="needs-validation" method="post" action="{{ action('DsorootController@edit', [$idDso]) }}">  
-                        @csrf
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                               
-                                <hr class="mb-4">
-                                
-                                <label for="date_demande">Date 1ère demande client</label>
-                                <input type="date" class="form-control" id="date_demande" name="date_demande" placeholder="" value="{{$data->date_demande}}" required>
+<section id="nouveau_dso">
+    <form method="POST" action="{{ action('DsorootController@store') }}">
+        @csrf
+        <div class="div_dso">
+            <div id="left">
+                <div class="nouveau-dso form-group">
+                    <label for="date_demande">Date 1ère demande client</label>
+                    <input type="date" class="form-control" id="date_demande" name="date_demande" placeholder="" value="{{$data->date_demande}}" required>
 
-                                <label for="date_rep">Date réponse souhaitée</label>
-                                <input type="date" class="form-control" id="date_rep" name="date_rep" placeholder="" value="{{$data->date_rep}}" required>
-
-                                <hr class="mb-4">
-                                
-                                <label for="date_envoi">Date d'envoi de l'offre</label>
-                                <input type="date" class="form-control" id="date_envoi" name="date_envoi" placeholder="" value="{{$data->date_envoi}}" required>
-
-                                <label for="date_livraison">Date livraison souhaitée</label>
-                                <input type="date" class="form-control" id="date_livraison" name="date_livraison" placeholder="" value="{{$data->date_livraison}}" required>
-
-                                
-                                <div class="py-3">
-                                    <button class="btn btn-primary btn-lg btn-block" type="submit">Continuer</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+                </div>
+                <div class="nouveau-dso form-group">
+                    <label for="date_rep">Date réponse souhaitée</label>
+                    <input type="date" class="form-control" id="date_rep" name="date_rep" placeholder="" value="{{$data->date_rep}}" required>
+                </div>
+                <div class="nouveau-dso form-group">
+                    <label for="date_envoi">Date d'envoi de l'offre</label>
+                    <input type="date" class="form-control" id="date_envoi" name="date_envoi" placeholder="" value="{{$data->date_envoi}}" required>
+                </div>
+                <div class="nouveau-dso form-group">
+                    <label for="date_livraison">Date livraison souhaitée</label>
+                    <input type="date" class="form-control" id="date_livraison" name="date_livraison" placeholder="" value="{{$data->date_livraison}}" required>
                 </div>
             </div>
+
+            <div id="right">
+                <div class="nouveau-dso form-group">
+                    <label for="exampleInputEmail1">Date création fiche :</label>
+                    <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $today }}" disabled>
+                </div>
+                <div class="nouveau-dso form-group">
+                    <label for="exampleInputPassword1">Nom du commercial :</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" value="{{ Auth::user()->name }}" disabled>
+                </div>
+                <button class="btn btn-warning" type="submit">Continuer</button>
+            </div>
+        </div>
+    </form>
+</section>
 @endsection
