723502b064ab6f753eac2ddb9fa8063026e96eb1
nico <nicocofer@hotmail.com>
2018-04-27 16:06:00 +0200

    suppresion du bouton pour générer ladmin pour la preprod

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 
 <section id="connexion">
-    <form method="POST" action="{{ route('addadmin') }}" style="position: absolute;top:0;left: 0;margin-top: -10px;">
+    {{-- <form method="POST" action="{{ route('addadmin') }}" style="position: absolute;top:0;left: 0;margin-top: -10px;">
         @csrf
         <button type="submit"></button>
-    </form>
+    </form> --}}
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
